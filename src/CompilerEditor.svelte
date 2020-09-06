1bed2b7e8f71dd1025b13599634cbf5449a5972f
王世安 <984292420@qq.com>
2020-09-06 12:43:51 +0800

    feat: 添加 高亮字体

--- src/CompilerEditor.svelte
+++ src/CompilerEditor.svelte
@@ -3,6 +3,28 @@
     import rangy from 'rangy/lib/rangy-selectionsaverestore';
     import MonkeyLexer from './monkey-lan/Lexer'
 
+    rangy.init()
+    let keyWordClass = 'keyword'
+    let keyWordElementArray = []
+    // change here
+    let identifierElementArray = []
+    let textNodeArray = []
+    let lineNodeClass = 'line'
+    let lineSpanNode = 'LineSpan'
+    let identifierClass = "Identifier"
+    let spanToTokenMap = {}
+    initPopoverControl()
+
+    let state: Record<string, any> = {}
+    state.popoverStyle = {}
+    state.popoverStyle.placement = "right"
+    state.popoverStyle.positionLeft = -100
+    state.popoverStyle.positionTop = -100
+    state.popoverStyle.content = ""
+
+    let keyToIgnore = ["Enter", " ", "ArrowUp", "ArrowDown",
+      "ArrowLeft", "ArrowRight"]
+
 </script>
 
 
